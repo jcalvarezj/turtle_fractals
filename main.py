1cd3630b6bc92447990d8fbcacd982cc2743a655
J. Alvarez <jcalvarezj@gmail.com>
2020-06-20 19:08:21 -0500

    Added color to Koch subproblems

--- main.py
+++ main.py
@@ -31,17 +31,22 @@ def initial_setup():
     window.setup(WIDTH + 4, HEIGHT + 8)
     window.setworldcoordinates(0, 0, WIDTH, HEIGHT)
     my_turtle.penup()
+    my_turtle.pensize(3)
     my_turtle.setposition(WIDTH/2, HEIGHT/2)
 
 
+def get_random_rgb():
+    return (randint(8, 255), randint(0, 250), randint(0, 205))
+
+
 def draw_fractal(fract_type):
+    colors = get_random_rgb()
     if fract_type == FractalType.CCIRCLES.value:
         draw_concentric_circles(400, 0.85)
-    elif fract_type == FractalType.BCIRCLES.value:
-        colors = (randint(0, 255), randint(0, 255), randint(0, 255))
+    elif fract_type == FractalType.BCIRCLES.value:        
         draw_binary_circles(WIDTH/2, HEIGHT/2, 400, colors)
     elif fract_type == FractalType.KOCH.value:
-        draw_koch_curve(WIDTH/2 - 400, HEIGHT/2, 800, 5)
+        draw_koch_curve(WIDTH/2 - 400, HEIGHT/2, 800, 5, colors)
     else:
         pass
 
@@ -61,7 +66,7 @@ def draw_circle(x, y, radius, colors):
 
 def draw_concentric_circles(radius, decrease_ratio):
     if (radius > 5):
-        color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        color = get_random_rgb()
         draw_circle(WIDTH/2, HEIGHT/2, radius, color)
         my_turtle.end_fill()
 
@@ -75,30 +80,33 @@ def draw_binary_circles(x, y, radius, level_color):
     if (radius >= 25):            
             draw_circle(x, y, radius, level_color)
 
-            new_color = (randint(0, 255), randint(0, 255), randint(0, 255))
+            new_color = get_random_rgb()
 
             draw_binary_circles(x - radius / 2, y, radius / 2, new_color)
             draw_binary_circles(x + radius / 2, y, radius / 2, new_color)
 
 
-def draw_line(x, y, length):
+def draw_line(x, y, length, color):
     my_turtle.setposition(x, y)
+    my_turtle.pencolor(color)
     my_turtle.pendown()
     my_turtle.forward(length)
     return my_turtle.pos()
 
 
-def draw_koch_curve(x, y, length, level):
+def draw_koch_curve(x, y, length, level, level_color):
     if (level < 1):
-        return draw_line(x, y, length)
+        return draw_line(x, y, length, level_color)
     else:
-        pos = draw_koch_curve(x, y, length/3, level - 2)
+        color1 = get_random_rgb()
+        pos = draw_koch_curve(x, y, length/3, level - 2, color1)
         my_turtle.left(60)
-        pos2 = draw_koch_curve(pos[0], pos[1], length/3, level - 1)
-        my_turtle.right(120)
-        pos3 = draw_koch_curve(pos2[0], pos2[1], length/3, level - 1)
-        my_turtle.left(60)
-        return draw_koch_curve(pos3[0], pos3[1], length/3, level - 2)
+        color2 = get_random_rgb()
+        pos2 = draw_koch_curve(pos[0], pos[1], length/3, level - 1, color2)
+        my_turtle.right(120)        
+        pos3 = draw_koch_curve(pos2[0], pos2[1], length/3, level - 1, color2)
+        my_turtle.left(60)        
+        return draw_koch_curve(pos3[0], pos3[1], length/3, level - 2, color1)
 
 
 if __name__ == "__main__":
